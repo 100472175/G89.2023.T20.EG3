c9d7bba1299eee895c6b9cd537aed99e57ec2bc7
100472175 <100472175@alumnos.uc3m.es>
2023-02-22 18:33:01 +0100

    Added the first calculator.py and test_calculator.py

--- calculator.py
+++ calculator.py
@@ -1,2 +1,13 @@
+class CalculatorException(Exception):
+    def __init__(self, value):
+        super().__init__(value)
+        self.value = value
+    def __str__(self):
+        return "Error" + self.value
+
 def sum(a,b):
-    pass # It does nothing
+    for i in (a,b):
+        if not isinstance(i, (int, float)):
+            raise CalculatorException("Invalid datatype in sum parameter")
+    return a + b
+
