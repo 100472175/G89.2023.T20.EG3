a91f252fa95e211f7d0b440ab2eeebc2f7005a72
100472175 <100472175@alumnos.uc3m.es>
2023-03-01 18:34:18 +0100

    New files, these are the necessary ones. With the Excel inside the docs folder

--- Bank/BankException.py
+++
@@ -1,5 +0,0 @@
-
-class BankException(ValueError):
-    def __init__(self, message, *args):
-        super(BankException, self).__init__(message, *args)
-
--- Class_unittest/Suma2.py
+++
@@ -1,5 +0,0 @@
-def suma(a, b):
-    for n in (a, b):
-        if not isinstance(n, int) and not isinstance(n, float):
-            raise TypeError
-    return a + b
