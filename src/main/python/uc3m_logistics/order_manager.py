e993615a95d53a26793c2e7fb9900ae71f147bde
SrS3R6IO <100472343@alumnos.uc3m.es>
2023-03-08 17:55:41 +0100

    Things done in class

--- src/main/python/uc3m_logistics/order_manager.py
+++ src/main/python/uc3m_logistics/order_manager.py
@@ -1,13 +1,12 @@
 """Module """
 import math
-
+from .order_request import OrderRequest
 
 class OrderManager:
     """Class for providing the methods for managing the orders"""
 
     def __init__(self):
         pass
-
     @staticmethod
     def validate_ean13(self, ean13):
         """
@@ -29,11 +28,12 @@ class OrderManager:
     def register_order(self, product_id, order_type, address, phone_number, zip_code):
         # Returns a string representing AM-FR-01-O1
         # On errors, returns a VaccineManagementException according to AM-FR-01-O2
-
-
-        pass
+        myorder = OrderRequest(product_id, order_type, address, phone_number, zip_code)
+        #if everything is ok, it will save into the file
+        return my_order.order_id
 
     def send_product (self, input_file):
+        pass
         # The input file is a string with the file path described in AM-FR-02-I1
         # Returns a String in hexadecimal which represents the tracking number (key that will be needed for AM-FR-02-O1 an AM-FR-02-02)
         # In case of error, it returns an OrderManagementException described in AM-FR-02-O3
--- src/unittest/python/test_register_order_tests.py
+++ src/unittest/python/test_register_order_tests.py
@@ -1,14 +1,17 @@
 """class for testing the regsiter_order method"""
 import unittest
-import freezegun
 from uc3m_logistics import OrderManager
+from freezegun import freeze_time
 
 class MyTestCase(unittest.TestCase):
     """class for testing the register_order method"""
     @freeze_time("2023-03-08")
     def test_something( self ):
         """dummy test"""
-        self.assertEqual(True, False)
+        my_manager = OrderManager
+        my_order_id = my_manager.register_order(product_id="8421691423220",order_type="REGULAR",address="C/LISBOA,4, MADRID, SPAIN",phone_number="654314159",zip_code="28005")
+
+        self.assertEqual(my_order_id, )
 
 
 if __name__ == '__main__':
