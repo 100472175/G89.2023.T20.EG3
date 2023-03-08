d335a1607cad02c356e77bbe6ca4b65757b625bc
100472175 <100472175@alumnos.uc3m.es>
2023-03-08 17:45:16 +0100

    Mid-class update

--- src/main/python/uc3m_logistics/order_manager.py
+++ src/main/python/uc3m_logistics/order_manager.py
@@ -1,8 +1,10 @@
 """Module """
 import math
 
+
 class OrderManager:
     """Class for providing the methods for managing the orders"""
+
     def __init__(self):
         pass
 
@@ -22,3 +24,16 @@ class OrderManager:
                 CheckSum += CurrentNumber
         Difference = 10 * math.ceil(CheckSum / 10) - CheckSum
         return int(ean13[-1]) == Difference
+
+
+    def register_order(self, product_id, order_type, address, phone_number, zip_code):
+        # Returns a string representing AM-FR-01-O1
+        # On errors, returns a VaccineManagementException according to AM-FR-01-O2
+
+
+        pass
+
+    def send_product (self, input_file):
+        # The input file is a string with the file path described in AM-FR-02-I1
+        # Returns a String in hexadecimal which represents the tracking number (key that will be needed for AM-FR-02-O1 an AM-FR-02-02)
+        # In case of error, it returns an OrderManagementException described in AM-FR-02-O3
--- src/unittest/python/test_register_order_tests.py
+++ src/unittest/python/test_register_order_tests.py
@@ -1,9 +1,11 @@
 """class for testing the regsiter_order method"""
 import unittest
+import freezegun
 from uc3m_logistics import OrderManager
 
 class MyTestCase(unittest.TestCase):
     """class for testing the register_order method"""
+    @freeze_time("2023-03-08")
     def test_something( self ):
         """dummy test"""
         self.assertEqual(True, False)
