24001f0e7ffdd0bc25abcd7d4c4b8c09430ed23c
SrS3R6IO <100472343@alumnos.uc3m.es>
2023-03-26 14:06:48 +0200

    Created all tests skeleton

---
+++ src/unittest/python/Send_Product tests/test_42_deleted.py
@@ -0,0 +1,31 @@
+import unittest
+import hashlib
+import os.path
+import unittest
+import json
+from uc3m_logistics import OrderManager, OrderRequest, OrderManagementException
+from freezegun import freeze_time
+from pathlib import Path
+from datetime import datetime
+
+class MyTestCase(unittest.TestCase):
+
+
+
+    @freeze_time("2023-03-08")
+    def test_42_deleted(self):
+        current_path = os.path.dirname(__file__)
+        current_path = current_path[:-34]
+        json_path = "main\JsonFiles"
+        current_path = os.path.join(current_path, json_path, "test_42_deleted.json")
+
+
+        with open(current_path, "r", encoding="utf-8") as file:            
+            
+
+        self.assertEqual(True, True)  # add assertion here
+
+
+if __name__ == "__main__":
+    unittest.main()
+
