4b4e7ad3ed922c0266f9c8ac9e72ca1e339180a4
Eduardo <100472175@alumnos.uc3m.es>
2023-03-27 23:34:32 +0200

    New simple tests

---
+++ src/unittest/python/Send_Product tests/test_46_modified.py
@@ -0,0 +1,22 @@
+import os.path
+import unittest
+import os
+from uc3m_logistics import OrderManager, OrderManagementException
+from freezegun import freeze_time
+
+
+class MyTestCase(unittest.TestCase):
+
+    @freeze_time("2023-03-08")
+    def test_46_modified(self):
+        current_path = os.path.dirname(__file__)
+        current_path = current_path[:-34]
+        json_path = "main/JsonFiles"
+        current_path = os.path.join(current_path, json_path, "test_46_modified.json")
+
+        with self.assertRaises(OrderManagementException) as exception:
+            OrderManager().send_product(current_path)
+            self.assertEqual(exception.exception.message, "JSON has not the expected stucture")
+
+if __name__ == "__main__":
+    unittest.main()
